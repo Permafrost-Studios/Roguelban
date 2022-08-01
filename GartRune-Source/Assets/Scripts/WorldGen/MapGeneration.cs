fca4a71fb66dc80141f9834e5ccc2e62442cac32
Julian <progamerguy005@gmail.com>
2022-08-01 19:42:32 +1000

    testing for bryan

--- GartRune-Source/Assets/Scripts/WorldGen/MapGeneration.cs
+++ GartRune-Source/Assets/Scripts/WorldGen/MapGeneration.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MapGeneration : MonoBehaviour
 {
-    public float mapSize = 1; //The number is the amount of chunks on each side of the square map (e.g. 2 would be 2 by 2 chunks)
+    public float mapSize = 10; //The number is the amount of chunks on each side of the square map (e.g. 2 would be 2 by 2 chunks)
 
     public float ChunkSize = 16;
 
@@ -13,7 +13,7 @@ public class MapGeneration : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GenerateChunk();
+        GenerateWorld(mapSize);
     }
 
     // Update is called once per frame
@@ -22,14 +22,25 @@ public class MapGeneration : MonoBehaviour
 
     }
 
-    void GenerateChunk() 
+    void GenerateWorld(float worldSize) 
+    {
+        for (float x = 1f; x < worldSize; x++) 
+        {
+            for (float z = 1f; z < worldSize; z++) 
+            {
+                GenerateChunk(x, z, ChunkSize);
+            }
+        }
+    }
+
+    void GenerateChunk(float chunkX, float chunkZ, float chunkSize) 
     {
         Debug.Log("Generating Chunk");
-        for (float x = 1; x < ChunkSize; x++) 
+        for (float x = 1f; x <= chunkSize; x++) 
         {
-            for (float z = 1; z < ChunkSize; z++) 
+            for (float z = 1f; z <= chunkSize; z++) 
             {
-                Instantiate(block, new Vector3(x, Mathf.PerlinNoise(x, z), z), block.transform.rotation);
+                Instantiate(block, new Vector3(x + (chunkX * ChunkSize), Mathf.PerlinNoise((x + (chunkX * ChunkSize)) / ChunkSize, (z + (chunkZ * ChunkSize)) / ChunkSize) * ChunkSize, z + (chunkZ * ChunkSize)), block.transform.rotation);
             }
         }
         Debug.Log("Done");
